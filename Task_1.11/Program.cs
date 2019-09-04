d2977d6285bb52f17293d35064c2027012a998d1
NikolaiMiroshnichenko <Nikolai.Miroshnichenko26@gmail.com>
2019-09-04 23:08:30 +0300

    Added tasks 10,11

---
+++ Task_1.11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            int i;
+            for (i = 0; i < 3; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int k = 0; k < 3; k++)
+            {
+                Console.WriteLine(arr[k]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
