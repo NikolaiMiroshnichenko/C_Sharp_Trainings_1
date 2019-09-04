36653a2456dea12ffb642bb930a1f3aef8c3c8a4
NikolaiMiroshnichenko <Nikolai.Miroshnichenko26@gmail.com>
2019-09-04 23:08:59 +0300

    1

---
+++ Task_1.10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task_1._10
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int[] nums = new int[3];
+
+            Console.WriteLine("Input 3 numbers");
+            int i;
+            for (i = 0; i < 3; i++)
+            {
+
+                try
+
+                {
+                    Console.Write("element - {0} : ", i);
+                    nums[i] = int.Parse(Console.ReadLine());
+
+                }
+                catch
+                {
+                    Console.WriteLine("Wrong input element № " + i);
+                    i--;
+                }
+            }
+            foreach (int n in nums)
+            {
+                Console.WriteLine(n);
+            }
+
+            Array.Reverse(nums);
+            Console.WriteLine("Reversed Array : ");
+            foreach (int v in nums)
+            {
+                Console.WriteLine(v);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
--- Task_1.12/Program.cs
+++ Task_1.12/Program.cs
@@ -4,18 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task_1._2
+namespace Task_1._12
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please, enter your name");
-            String name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name);
-
-       
-        Console.ReadKey();
         }
     }
 }
